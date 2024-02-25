0e96d17b1d3590923f2dc85316d47ca8cecb5489
Tristan H <tristanskyhook@gmail.com>
2024-02-25 13:52:18 -0800

    dz

---
+++ components/images.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { IconImgProps } from "@/types";
+import { useTheme } from "next-themes";
+
+
+export const PoppyIconSmall = (props: IconImgProps) => {
+	const { theme, setTheme } = useTheme();
+  
+
+	const lightLogo = "/logoSmallLight.png";
+	const darkLogo = "/logoSmallDark.png";
+
+	return (
+		<img
+			alt="Small logo"
+			src={theme === "light" ? darkLogo : lightLogo}
+			width={props.size}
+			height={props.size}
+			{...props}
+		/>
+	);
+}
+export const PoppyIconLarge = (props: IconImgProps) => {
+	const { theme, setTheme } = useTheme();
+  
+
+	const lightLogo = "/logoLight.png";
+	const darkLogo = "/logoDark.png";
+
+	return (
+		<img
+			alt="Large logo"
+			src={theme === "light" ? darkLogo : lightLogo}
+			width={props.size}
+			height={props.size}
+			{...props}
+		/>
+	);
+}
